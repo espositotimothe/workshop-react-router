e2296a0d035c39a37c2fe1d338171b9c6e9dfdf5
TIMOTHE ESPOSITO <timotheesposito@gmail.com>
2024-10-30 10:45:17 +0100

    newchg

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,16 @@
-import { useState } from "react";
+import { Link, Outlet } from "react-router-dom";
 
 import "./App.css";
 
-
-import Home from "./pages/Home";
-import About from "./pages/About";
-
-
 function App() {
-  const [currentLocation, setCurrentLocation] = useState("/");
-
   return (
     <>
       <nav>
-        <button onClick={() => setCurrentLocation("/")} type="button">
-          Home
-        </button>
-        <button onClick={() => setCurrentLocation("/about")} type="button">
-          About
-        </button>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
       </nav>
       <main>
-        {currentLocation === "/" && <Home />}
-        {currentLocation === "/about" && <About />}
+        <Outlet />
       </main>
     </>
   );
